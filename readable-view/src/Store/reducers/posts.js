507f005d8bfba517c72f337bf32b694f23a74379
Alexandre Rocha <alexandrececchin@gmail.com>
2019-01-06 15:06:41 -0200

    Add post reducer and actions functionality

---
+++ readable-view/src/Store/reducers/posts.js
@@ -0,0 +1,23 @@
+import * as constants from "../../Util/constant";
+
+export default function users(state = {}, action) {
+    switch (action.type) {
+        case constants.RECEIVE_POST:
+            return { ...state, ...action.posts };
+        case constants.ADD_POST:
+        case constants.UPDATE_POST:
+            const { post } = action;
+            return {
+                ...state,
+                [action.post.id]: post
+            }
+        case constants.DELETE_POST:
+            const { id } = action.id;
+            const posts = state.filter(post => post.id !== id);
+
+            return { ...state, posts }
+
+        default:
+            return state;
+    }
+}
