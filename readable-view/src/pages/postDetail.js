75c8fc3591747582a3fa82d0c84db938c222d159
Alexandre Rocha <alexandrececchin@gmail.com>
2019-02-05 11:08:27 -0200

    Fix redirect Bug

--- readable-view/src/pages/postDetail.js
+++ readable-view/src/pages/postDetail.js
@@ -1,63 +1,66 @@
 import React, { Component } from 'react';
-import { withRouter, Redirect } from 'react-router-dom'
+import { withRouter, Redirect } from 'react-router-dom';
 import Post from '../components/posts/post/post';
-import Comments from '../components/comments/comments'
+import Comments from '../components/comments/comments';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Creators as PostActions } from '../redux/post/postActions';
-import { Creators as CommentActions } from "../redux/comment/commentActions";
-import { Selectors } from "../redux/rootReducer";
-
+import { Creators as CommentActions } from '../redux/comment/commentActions';
+import { Selectors } from '../redux/rootReducer';
 
 class postDetail extends Component {
-    componentDidMount() {
-        const { category, id } = this.props.match.params;
-        const { fetchSinglePostRequest, fetchCommentsRequest } = this.props;
-        fetchSinglePostRequest(id, category);
-        fetchCommentsRequest(id);
+  componentDidMount() {
+    const { category, id } = this.props.match.params;
+    const { fetchSinglePostRequest, fetchCommentsRequest } = this.props;
+    fetchSinglePostRequest(id, category);
+    fetchCommentsRequest(id);
+  }
+
+  render() {
+    const id = this.props.match.params.id;
+    const { commentsToRender, redirectHome } = this.props;
+    if (redirectHome) {
+      return <Redirect to="/404" />;
     }
 
-    render() {
-        const id = this.props.match.params.id;
-        const { commentsToRender, redirectHome } = this.props;
-        if (redirectHome) {
-            return <Redirect to="/" />
-        }
-
-        return (
-            <div className="col-md-10 col-md-offset-1" >
-                <div className="box-footer box-comments">
-                    <Post id={id} />
-                    <Comments postId={id} comments={commentsToRender} />
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="col-md-10 col-md-offset-1">
+        <div className="box-footer box-comments">
+          <Post id={id} />
+          <Comments postId={id} comments={commentsToRender} />
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch =>
-    bindActionCreators({ ...PostActions, ...CommentActions }, dispatch);
+  bindActionCreators({ ...PostActions, ...CommentActions }, dispatch);
 
 function mapStateToProps(state, props) {
-    const { category, id } = props.match.params;
+  const { category, id } = props.match.params;
 
-    let commentsToRender = Selectors.comments.getVisibleComments(state, id)
+  let commentsToRender = Selectors.comments.getVisibleComments(state, id);
 
-    let post = Selectors.posts.getPost(state, id)
+  let post = Selectors.posts.getPost(state, id);
+  console.log('object :', post);
+  let redirectHome = false;
 
-    let redirectHome = false;
+  if (post && post.deleted) {
+    redirectHome = true;
+  }
 
-    if (post && post.deleted) {
-        redirectHome = true
-    }
-
-    return {
-        category,
-        id,
-        commentsToRender,
-        redirectHome
-    }
+  return {
+    category,
+    id,
+    commentsToRender,
+    redirectHome
+  };
 }
 
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(postDetail));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(postDetail)
+);
--- readable-view/src/redux/post/postSaga.js
+++ readable-view/src/redux/post/postSaga.js
@@ -2,28 +2,20 @@ import { call, put } from 'redux-saga/effects';
 import { normalize } from 'normalizr';
 import api from '../../service/service';
 import { post as postSchema } from './postSchema';
-import { Creators as PostsActions } from "./postActions";
+import { Creators as PostsActions } from './postActions';
 
 export function* fetchPosts(action) {
   const { category } = action.payload;
-  let url = category ? `/${category}/posts` : '/posts'
+  let url = category ? `/${category}/posts` : '/posts';
 
   try {
-
     const response = yield call(api.get, url);
 
-    yield put(
-      PostsActions.fetchPostsSuccess(normalize(response.data, [postSchema]), category)
-    );
+    yield put(PostsActions.fetchPostsSuccess(normalize(response.data, [postSchema]), category));
   } catch (err) {
-    yield put(
-      PostsActions.fetchPostsError(
-        'An error has occurred. Please, refresh the page.',
-      ),
-    );
+    yield put(PostsActions.fetchPostsError('An error has occurred. Please, refresh the page.'));
   }
 }
-
 
 export function* fetchPost(action) {
   const { id, category } = action.payload;
@@ -31,15 +23,15 @@ export function* fetchPost(action) {
 
   try {
     const response = yield call(api.get, url);
-
-    yield put(
-      PostsActions.fetchSinglePostSuccess(normalize(response.data, postSchema), category)
-    );
+    let data = response.data;
+    //IF Api doesn't return anything to post, create fake object just to redirect to 404 page
+    if (Object.keys(data).length === 0 && data.constructor === Object) {
+      data = { id, deleted: true };
+    }
+    yield put(PostsActions.fetchSinglePostSuccess(normalize(data, postSchema), category));
   } catch (err) {
     yield put(
-      PostsActions.fetchSinglePostError(
-        'An error has occurred. Please, refresh the page.',
-      ),
+      PostsActions.fetchSinglePostError('An error has occurred. Please, refresh the page.')
     );
   }
 }
@@ -47,39 +39,29 @@ export function* fetchPost(action) {
 export function* registerVotePost(action) {
   const { postId, option } = action.payload;
 
-
   try {
     const response = yield call(api.post, `/posts/${postId}`, { option });
-    yield put(
-      PostsActions.registerPostVoteSuccess(normalize(response.data, postSchema), postId),
-    );
+    yield put(PostsActions.registerPostVoteSuccess(normalize(response.data, postSchema), postId));
   } catch (err) {
     yield put(
-      PostsActions.registerPostVoteError(
-        'An error has occurred. Please, refresh the page.',
-      ),
+      PostsActions.registerPostVoteError('An error has occurred. Please, refresh the page.')
     );
   }
 }
-
 
 export function* updatePost(action) {
   const { title, body, postId } = action.payload;
   const params = {
     title,
-    body,
+    body
   };
 
   try {
     const response = yield call(api.put, `/posts/${postId}`, { ...params });
-console.log('response :', normalize(response.data, postSchema));
+    console.log('response :', normalize(response.data, postSchema));
     yield put(PostsActions.updatePostSuccess(normalize(response.data, postSchema)));
   } catch (err) {
-    yield put(
-      PostsActions.updatePostError(
-        'An error has occurred. Please, refresh the page.',
-      ),
-    );
+    yield put(PostsActions.updatePostError('An error has occurred. Please, refresh the page.'));
   }
 }
 
@@ -90,28 +72,18 @@ export function* deletePost(action) {
     console.log('response :', normalize(response.data, postSchema));
     yield put(PostsActions.deletePostSuccess(normalize(response.data, postSchema)));
   } catch (err) {
-    yield put(
-      PostsActions.deletePostError(
-        'An error has occurred. Please, refresh the page.',
-      ),
-    );
+    yield put(PostsActions.deletePostError('An error has occurred. Please, refresh the page.'));
   }
 }
 
 export function* addPost(action) {
-    const { post } = action.payload;
+  const { post } = action.payload;
 
-    try {
-      const response = yield call(api.post, '/posts', { ...post });
+  try {
+    const response = yield call(api.post, '/posts', { ...post });
 
-      yield put(
-        PostsActions.addPostSuccess(normalize(response.data, postSchema)),
-      );
-    } catch (err) {
-      yield put(
-        PostsActions.addPostError(
-          'An error has occurred. Please, refresh the page.',
-        ),
-      );
-    }
+    yield put(PostsActions.addPostSuccess(normalize(response.data, postSchema)));
+  } catch (err) {
+    yield put(PostsActions.addPostError('An error has occurred. Please, refresh the page.'));
   }
+}
